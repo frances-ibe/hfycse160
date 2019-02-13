5e9d8128d7a4616488b1bbb41093d9740d5f787a
Nathaniel Linden <natelinden@D-10-19-181-161.dhcp4.washington.edu>
2019-02-13 13:00:00 -0800

    Working version of the code. Passing in attributes as a tuple is ugly, maybe better to do as a list

--- code/jsonToPython.py
+++ code/jsonToPython.py
@@ -13,8 +13,8 @@ import json
 # update list
 # return list
 
-# this is fun
-def jsonToDict(fileName, attributes):
+
+def jsonToDictList(fileName, attributes):
     """Converts .json file to python list of dictionaries.
 
     fileName - the .json file to be converted
@@ -27,13 +27,38 @@ def jsonToDict(fileName, attributes):
     # check fileName is .json
     assert fileName[-5:] == '.json', 'The file inputted is not .json' + fileName
 
-    jsonDict = {}
-    for object in open(fileName, 'r'):
+    dictList = []  # create empty list to store object dictionaries
+    for object in open(fileName, 'r'):  # loop through .json objects
         objDict = json.loads(object)  # load on object from .json file as dict
-        parsedObjDict = parseDict(objDict, attributes)
+        parsedObjDict = _parseDict(objDict, attributes)  # parse for attributes
+        # parsedObjDict will  be none if objDict doesn not contain attributes
+        if parsedObjDict is not None:
+            dictList.append(parsedObjDict)  # update the dict list
+
+    return dictList
 
 
-def parseDict(dictionary, attributes):
-    
+def _parseDict(dictionary, attributes):
+    """ Private funciton which returns a dictionary containing the key values
+    pairs from dictionary with keys corresponding to the items in attributes.
 
-    return jsonDict
+    Returns a dictionary if all of the attributes are keys in the dicitonary
+    Returns None if an attribute is not contained in the dictionary
+
+    To Do: fix conversion of attributes tuple to list 2/13/19
+    """
+    # convert attributes to a list
+    if len(attributes) == 1:  # note this is an ugly solution to a problem with convert
+                     # from a tuple to a list, may just be able to pass in an
+                     # attribute list instead
+        keysList = [attributes]
+    else:
+        keysList = list(attributes)
+    print(keysList)
+    try:
+        # use list comprehension to return list of values from the attributes
+        valuesList = [dictionary[key] for key in keysList]
+    except KeyError:  # if a given attribute not in the dictionary return None
+        return None
+
+    return dict(zip(keysList, valuesList))  # return a dictionary
