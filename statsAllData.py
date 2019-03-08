f1e31b905d5764c53fc084211cde403e5404c642
pgrosjean <parker.grosjean@gmail.com>
2019-03-07 20:15:32 -0800

    RQ 3 and RQ 1 figures generated and outlier removed RQ1 analysis added.

--- statsAllData.py
+++ statsAllData.py
@@ -7,13 +7,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Load Data
+# Loading Preprocessed Data
+# loading zillow data
 zillowData = pd.read_csv("vegasHousing.csv")
 zillowData.sort_values(by=["postalCode"])
 
+# loading irs data
 irsData = pd.read_csv("vegasIRS.csv")
 irsData.sort_values(by=["postalCode"])
 
+# loading yelp data
 yelpData = pd.read_csv("vegasRest.csv")
 yelpData.sort_values(by=["postalCode"])
 ### Note: the yelp data with city="Las Vegas" appears to have some mislabeled
@@ -27,27 +30,78 @@ for zip in irsData["postalCode"]:
     d.append({"postalCode": zip, "avgPrice": np.mean(zipPrice)})
 yelpAvg = pd.DataFrame(d)
 
-#merge yelp and zillow data
+# merge yelp and zillow data
 yzi = pd.merge(irsData, zillowData[['postalCode', 'zhvi']], on='postalCode')
 yzi = pd.merge(yzi, yelpAvg[['postalCode', 'avgPrice']], on='postalCode')
 yziFilt = yzi[["income", "zhvi", "avgPrice"]]
 
-# # Compute Correlation Statistics Between household income and median home value
-# # Will utilze the IRS dataset
+# Removing the Vegas Strip as it is an outlier in our data
+yziNoOutlier = yzi[yzi["avgPrice"] != max(yzi["avgPrice"])]
+yziNoOutlier.dropna()
+
+
+#
+# # ## Research Question 1
+# # Does restaurant star rating across price point level vary for
+# # different zip code  areas? What are trends in such variance across
+# # zip code areas and how does it relate to socioeconomic factors
+# # such as median house value and average household income?
+#
+
+
+## Compute Correlation Statistics Between household income and median home value
+# no outlier removed
 corrDF = yziFilt.corr(method='pearson')
 # print(corrDF)
 
-# # Plot Grid of Scatter Plots
-# fig1 = plt.figure()corrDF = yziFilt.corr(method='pearson')
-# # plt.subplot(3,1,1)
-# plt.plot(yzi["zhvi"], yzi["avgPrice"],'.')
-# # plt.subplot(3,1,2)
-# fig2 = plt.figure()
-# plt.plot(yzi["zhvi"], yzi["income"],'.')
-# # plt.subplot(3,1,3)
-# fig3 = plt.figure()
-# plt.plot(yzi["income"], yzi["avgPrice"],'.')
-# plt.show()
+
+## Compute Correlation Statistics Between household income and median home value
+# no outlier removed
+corrDF2 = yziNoOutlier.corr(method='pearson')
+# print(corrDF2)
+
+
+## Plotting Grid of Scatter Plots for RQ1
+figRQ1, axRq1 = plt.subplots(2, 3)
+figRQ1.set_size_inches(12, 5)
+plt.subplots_adjust(hspace=0.5, wspace=0.5)
+axRq1[0,0].scatter(yzi["zhvi"], yzi["avgPrice"], s=10)
+axRq1[0,2].scatter(yzi["zhvi"], yzi["income"], s=10)
+axRq1[0,1].scatter(yzi["income"], yzi["avgPrice"], s=10)
+axRq1[0,0].set_ylabel("Average \n Price Point", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[0,0].set_xlabel("zhvi", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[0,2].set_ylabel("Average Income", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[0,2].set_xlabel("zhvi", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[0,1].set_ylabel("Average \n Price Point", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[0,1].set_xlabel("Average Income", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[0,0].set_ylim((0,4))
+axRq1[0,0].set_xlim(((150000,500000)))
+axRq1[0,2].set_ylim((25000,225000))
+axRq1[0,2].set_xlim(((150000,500000)))
+axRq1[0,1].set_ylim((0,4))
+axRq1[0,1].set_xlim(((25000,225000)))
+axRq1[0,0].set_title('No Outlier Removed', fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[0,1].set_title('No Outlier Removed', fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[0,2].set_title('No Outlier Removed', fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[1,0].scatter(yziNoOutlier["zhvi"], yziNoOutlier["avgPrice"], s=10)
+axRq1[1,2].scatter(yziNoOutlier["zhvi"], yziNoOutlier["income"], s=10)
+axRq1[1,1].scatter(yziNoOutlier["income"], yziNoOutlier["avgPrice"], s=10)
+axRq1[1,0].set_ylabel("Average \n Price Point", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[1,0].set_xlabel("zhvi", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[1,2].set_ylabel("Average Income", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[1,2].set_xlabel("zhvi", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[1,1].set_ylabel("Average \n Price Point", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[1,1].set_xlabel("Average Income", fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[1,0].set_ylim((0,4))
+axRq1[1,0].set_xlim(((150000,500000)))
+axRq1[1,2].set_ylim((25000,225000))
+axRq1[1,2].set_xlim(((150000,500000)))
+axRq1[1,1].set_ylim((0,4))
+axRq1[1,1].set_xlim(((25000,225000)))
+axRq1[1,0].set_title('Outlier Removed', fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[1,1].set_title('Outlier Removed', fontname="Arial", fontsize=12, fontweight='bold')
+axRq1[1,2].set_title('Outlier Removed', fontname="Arial", fontsize=12, fontweight='bold')
+# plt.savefig('RQ1_scatters.png')
 
 #
 # # ## Research Question 3
@@ -55,6 +109,8 @@ corrDF = yziFilt.corr(method='pearson')
 # # What are trends in such variance across zip code areas and how does it relate to
 # # socioeconomic factors such as median house value and average household income? """
 #
+
+## Data preprocessing for RQ 3 analysis
 avgRatingByZip = []  # empty list to hold dictionaries of avg rating per price pnt for each zip code
 numRestByZip = []  # empty list to hold dictionaries of num rest per price pnt for each zip code
 
@@ -75,6 +131,7 @@ for zip in irsData["postalCode"]:  # loop through zip codes
 avgRatingByZipDF = pd.DataFrame(avgRatingByZip)  # create dataframe
 numRestByZipDF = pd.DataFrame(numRestByZip)  # create dataframe
 
+
 restRatio = []
 for zip in irsData["postalCode"]:
     priceLevel = [1, 2, 3, 4]
@@ -85,85 +142,143 @@ for zip in irsData["postalCode"]:
                 3: ratios[2], 4: ratios[3]})
 restRatio = pd.DataFrame(restRatio)
 
-avgRatingByZipDF = pd.DataFrame(avgRatingByZip)  # create dataframe
-numRestByZipDF = pd.DataFrame(numRestByZip)  # create dataframe
 
-
-avgRatingByZipDF = pd.DataFrame(avgRatingByZip)  # create dataframes
-numRestByZipDF = pd.DataFrame(numRestByZip)  # create dataframes
-avgRatingByZipDF = pd.DataFrame(avgRatingByZip)  # create dataframe
-numRestByZipDF = pd.DataFrame(numRestByZip)  # create dataframe
-
-
-## Calculate Correlation
+## Calculating Correlation for avg rating yelp at different price points
+# vs average income and for restaurant proportion at different price points
+# vs average income
 aRIncome = pd.merge(irsData, avgRatingByZipDF, on='postalCode')
 rRIncome = pd.merge(irsData, restRatio, on='postalCode')
-
 aRICorr = aRIncome[[1, 2, 3, 4, "income"]].corr(method='pearson')
 # print(aRICorr)
 rRICorr = rRIncome[[1, 2, 3, 4, "income"]].corr(method='pearson')
 # print(rRICorr)
 
+
+## Plotting avg rating yelp at different price points
+# vs average income
 figAvgI, axAvgI = plt.subplots(2, 2, sharex='col', sharey='row')
-axAvgI[0, 0].scatter(aRIncome["income"], aRIncome[1], s=5)
-axAvgI[0, 1].scatter(aRIncome["income"], aRIncome[2], s=5)
-axAvgI[1, 0].scatter(aRIncome["income"], aRIncome[3], s=5)
-axAvgI[1, 1].scatter(aRIncome["income"], aRIncome[4], s=5)
-# plt.show()
+figAvgI.set_size_inches(8, 5)
+plt.subplots_adjust(hspace=0.2, wspace=0.2)
+axAvgI[0, 0].scatter(aRIncome["income"], aRIncome[1], s=10)
+axAvgI[0, 1].scatter(aRIncome["income"], aRIncome[2], s=10)
+axAvgI[1, 0].scatter(aRIncome["income"], aRIncome[3], s=10)
+axAvgI[1, 1].scatter(aRIncome["income"], aRIncome[4], s=10)
+axAvgI[0,0].set_ylim((0,5))
+axAvgI[1,0].set_ylim((0,5))
+axAvgI[0,1].set_ylim((0,5))
+axAvgI[1,1].set_ylim((0,5))
+axAvgI[0,0].set_xlim((25000,225000))
+axAvgI[0,1].set_xlim((25000,225000))
+axAvgI[1,0].set_xlim((25000,225000))
+axAvgI[1,1].set_xlim((25000,225000))
+axAvgI[0,0].set_title('Price Point 1', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgI[0,1].set_title('Price Point 2', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgI[1,0].set_title('Price Point 3', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgI[1,1].set_title('Price Point 4', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgI[0,0].set_ylabel('Average Rating', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgI[1,0].set_xlabel('Mean Income', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgI[1,0].set_ylabel('Average Rating', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgI[1,1].set_xlabel('Mean Income', fontname="Arial", fontsize=12, fontweight='bold')
+# plt.savefig('scatter_income_avgrating_by_ppoint.png')
 
-figNumI, axNumI = plt.subplots(2, 2, sharex='col', sharey='row')
-axNumI[0, 0].scatter(rRIncome["income"], rRIncome[1], s=5)
-axNumI[0, 1].scatter(rRIncome["income"], rRIncome[2], s=5)
-axNumI[1, 0].scatter(rRIncome["income"], rRIncome[3], s=5)
-axNumI[1, 1].scatter(rRIncome["income"], rRIncome[4], s=5)
-# plt.show()
 
-###### Needs to be fixed #######
+# Plotting restaurant proportion at different price points
+# vs average income
+figRestI, axRestI = plt.subplots(2, 2, sharex='col', sharey='row')
+figRestI.set_size_inches(8, 5)
+plt.subplots_adjust(hspace=0.2, wspace=0.2)
+axRestI[0, 0].scatter(rRIncome["income"], rRIncome[1], s=10)
+axRestI[0, 1].scatter(rRIncome["income"], rRIncome[2], s=10)
+axRestI[1, 0].scatter(rRIncome["income"], rRIncome[3], s=10)
+axRestI[1, 1].scatter(rRIncome["income"], rRIncome[4], s=10)
+axRestI[0,0].set_ylim((0,0.8))
+axRestI[0,1].set_ylim((0,0.8))
+axRestI[1,0].set_ylim((0,0.8))
+axRestI[1,1].set_ylim((0,0.8))
+axRestI[0,0].set_xlim((25000,225000))
+axRestI[0,1].set_xlim((25000,225000))
+axRestI[1,0].set_xlim((25000,225000))
+axRestI[1,1].set_xlim((25000,225000))
+axRestI[0,0].set_title('Price Point 1', fontname="Arial", fontsize=12, fontweight='bold')
+axRestI[0,1].set_title('Price Point 2', fontname="Arial", fontsize=12, fontweight='bold')
+axRestI[1,0].set_title('Price Point 3', fontname="Arial", fontsize=12, fontweight='bold')
+axRestI[1,1].set_title('Price Point 4', fontname="Arial", fontsize=12, fontweight='bold')
+axRestI[0,0].set_ylabel('Restaurant Proportion', fontname="Arial", fontsize=12, fontweight='bold')
+axRestI[1,0].set_xlabel('Mean Income', fontname="Arial", fontsize=12, fontweight='bold')
+axRestI[1,0].set_ylabel('Restaurant Proportion', fontname="Arial", fontsize=12, fontweight='bold')
+axRestI[1,1].set_xlabel('Mean Income', fontname="Arial", fontsize=12, fontweight='bold')
+# plt.savefig('scatter_income_proprest_by_ppoint.png')
+
+
+# Calculating Correlation
+aRZil = pd.merge(zillowData, avgRatingByZipDF, on='postalCode')
+rRZil = pd.merge(zillowData, restRatio, on='postalCode')
+aRZCorr = aRZil[[1, 2, 3, 4, "zhvi"]].corr(method='pearson')
+# print(aRZCorr)
+rRZCorr = rRZil[[1, 2, 3, 4, "zhvi"]].corr(method='pearson')
+# print(rRZCorr)
+
 
 # Plotting avg rating yelp at different price points
 # vs Median household income
-aRZil = pd.merge(zillowData, avgRatingByZipDF, on='postalCode')
-
 figAvgZ, axAvgZ = plt.subplots(2,2, sharex='col', sharey='row')
-axAvgZ[0, 0].scatter(aRZil["zhvi"], aRZil[1], s=5)
-axAvgZ[0, 1].scatter(aRZil["zhvi"], aRZil[2], s=5)
-axAvgZ[1, 0].scatter(aRZil["zhvi"], aRZil[3], s=5)
-axAvgZ[1, 1].scatter(aRZil["zhvi"], aRZil[4], s=5)
-plt.show()
+figAvgZ.set_size_inches(8, 5)
+plt.subplots_adjust(hspace=0.2, wspace=0.2)
+axAvgZ[0, 0].scatter(aRZil["zhvi"], aRZil[1], s=10)
+axAvgZ[0, 1].scatter(aRZil["zhvi"], aRZil[2], s=10)
+axAvgZ[1, 0].scatter(aRZil["zhvi"], aRZil[3], s=10)
+axAvgZ[1, 1].scatter(aRZil["zhvi"], aRZil[4], s=10)
+axAvgZ[0,0].set_ylim((0,5))
+axAvgZ[1,0].set_ylim((0,5))
+axAvgZ[0,1].set_ylim((0,5))
+axAvgZ[1,1].set_ylim((0,5))
+axAvgZ[0,0].set_xlim((150000,500000))
+axAvgZ[0,1].set_xlim((150000,500000))
+axAvgZ[1,0].set_xlim((150000,500000))
+axAvgZ[1,1].set_xlim((150000,500000))
+axAvgZ[0,0].set_title('Price Point 1', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgZ[0,1].set_title('Price Point 2', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgZ[1,0].set_title('Price Point 3', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgZ[1,1].set_title('Price Point 4', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgZ[0,0].set_ylabel('Average Rating', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgZ[1,0].set_xlabel('zhvi', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgZ[1,0].set_ylabel('Average Rating', fontname="Arial", fontsize=12, fontweight='bold')
+axAvgZ[1,1].set_xlabel('zhvi', fontname="Arial", fontsize=12, fontweight='bold')
+# plt.savefig('scatter_zhvi_avgrating_by_ppoint.png')
 
-# avgRateVsZhvi = plt.figure()
-# plt.scatter(yzi["zhvi"], avgRatingByZipDF[1])
-# plt.title('Price Point 1', fontname="Arial", fontsize=12)
-# plt.ylabel('Average Rating', fontname="Arial", fontsize=12)
-# plt.xlabel('Median Estimated Home Value (zhvi)', fontname="Arial", fontsize=12)
-# plt.ylim((0,6))
-# plt.subplot(4,1,2)
-# plt.scatter(yzi["zhvi"], avgRatingByZipDF[2])
-# plt.title('Price Point 2', fontname="Arial", fontsize=12)
-# plt.ylabel('Average Rating', fontname="Arial", fontsize=12)
-# plt.xlabel('Median Estimated Home Value (zhvi)', fontname="Arial", fontsize=12)
-# plt.ylim((0,6))
-# plt.subplot(4,1,3)
-# plt.scatter(yzi["zhvi"], avgRatingByZipDF[3])
-# plt.title('Price Point 3', fontname="Arial", fontsize=12)
-# plt.ylabel('Average Rating', fontname="Arial", fontsize=12)
-# plt.xlabel('Median Estimated Home Value (zhvi)', fontname="Arial", fontsize=12)
-# plt.ylim((0,6))
-# plt.subplot(4,1,4)
-# plt.scatter(yzi["zhvi"], avgRatingByZipDF[4])
-# plt.title('Price Point 4', fontname="Arial", fontsize=12)
-# plt.ylabel('Average Rating', fontname="Arial", fontsize=12)
-# plt.xlabel('Median Estimated Home Value (zhvi)', fontname="Arial", fontsize=12)
-# plt.ylim((0,6))
-# plt.show()
-#
-# avgRatingZhviDF = pd.merge(avgRatingByZipDF, yzi[["postalCode","zhvi"]], on="postalCode")
-# corrRatingZhviDF = avgRatingZhviDF.corr(method='pearson') # generating correlation matrix
-# print(corrRatingZhviDF) # printing correlation matrix
-#
-# # generating comparative boxplots for weighted average clustered by price point
-# bxPlt_nw = plt.figure()
-# bxPlt1 = avgRatingZhviDF.boxplot(column=[1,2,3,4])
-# plt.ylabel('Average Rating', fontname="Arial", fontsize=12)
-# plt.xlabel('Price Point', fontname="Arial", fontsize=12)
-# plt.show()
+# Plotting restaurant proportion at different price points
+# vs Median household income
+figRestZ, axRestZ = plt.subplots(2, 2, sharex='col', sharey='row')
+figRestZ.set_size_inches(8, 5)
+plt.subplots_adjust(hspace=0.2, wspace=0.2)
+axRestZ[0, 0].scatter(rRZil["zhvi"], rRZil[1], s=10)
+axRestZ[0, 1].scatter(rRZil["zhvi"], rRZil[2], s=10)
+axRestZ[1, 0].scatter(rRZil["zhvi"], rRZil[3], s=10)
+axRestZ[1, 1].scatter(rRZil["zhvi"], rRZil[4], s=10)
+axRestZ[0,0].set_ylim((0,0.8))
+axRestZ[0,1].set_ylim((0,0.8))
+axRestZ[1,0].set_ylim((0,0.8))
+axRestZ[1,1].set_ylim((0,0.8))
+axRestZ[0,0].set_xlim((150000,500000))
+axRestZ[0,1].set_xlim((150000,500000))
+axRestZ[1,0].set_xlim((150000,500000))
+axRestZ[1,1].set_xlim((150000,500000))
+axRestZ[0,0].set_title('Price Point 1', fontname="Arial", fontsize=12, fontweight='bold')
+axRestZ[0,1].set_title('Price Point 2', fontname="Arial", fontsize=12, fontweight='bold')
+axRestZ[1,0].set_title('Price Point 3', fontname="Arial", fontsize=12, fontweight='bold')
+axRestZ[1,1].set_title('Price Point 4', fontname="Arial", fontsize=12, fontweight='bold')
+axRestZ[0,0].set_ylabel('Restaurant Proportion', fontname="Arial", fontsize=12, fontweight='bold')
+axRestZ[1,0].set_xlabel('zhvi', fontname="Arial", fontsize=12, fontweight='bold')
+axRestZ[1,0].set_ylabel('Restaurant Proportion', fontname="Arial", fontsize=12, fontweight='bold')
+axRestZ[1,1].set_xlabel('zhvi', fontname="Arial", fontsize=12, fontweight='bold')
+# plt.savefig('scatter_zhvi_proprest_by_ppoint.png')
+
+
+# generating comparative boxplots for average rating clustered by price point
+avgRatingZhviDF = pd.merge(avgRatingByZipDF, yzi[["postalCode","zhvi"]], on="postalCode")
+bxPlt_nw = plt.figure()
+bxPlt1 = avgRatingZhviDF.boxplot(column=[1,2,3,4])
+plt.ylabel('Average Rating', fontname="Arial", fontsize=12, fontweight='bold')
+plt.xlabel('Price Point', fontname="Arial", fontsize=12, fontweight='bold')
+# plt.savefig('comp_boxplot_ppoint_avgrating.png')
+#plt.show()
