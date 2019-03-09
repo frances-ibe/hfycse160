92e83507f34967fbe559c7c67bff0f895e9d97c5
Nathaniel Linden <natelinden@Nathaniels-MacBook-Pro-2.local>
2019-03-08 23:01:31 -0800

    redo work that got lost

--- dataPreprocessingScript.py
+++ dataPreprocessingScript.py
@@ -1,13 +1,32 @@
-# Testing ZillowPreProcess
+""" The following code performs ALL statistical testing for the completion of
+our CSE 160 final project. All supporting code and preprocessed data will be
+made available.
 
+Authors: Frances Ingram-Bate, Parker Grosjean, Nathaniel Linden
+
+Updated: March 7th 2019
+
+Abbreviations (We will use these throughout the script):
+-  zhvi - zillow homw value index
+-  avgPrice - average restuarant price
+-  yzi - yelp, zillow, IRS
+
+Note: This code is requrires the preprocessed data files, alongside the geoJSON
+file in the same directory in order to compile.
+"""
 import preProcessZillow as ppz
 import preProcessIRS as ppi
 import preProcessYelp as ppy
 import pandas as pd
 
 
+# preProcess zillow
 ppz.zillPreProcess("Zip_Zhvi_Summary_AllHomes.csv", targetCity = "Las Vegas")
+
+# preProcess IRS
 irsDict = ppi.preProcess("16zp29nv.csv", ppz.zipcodeList())
 df = ppz.dictToCSVDF(irsDict, cols=('postalCode', 'income'))
 df.to_csv('vegasIRS.csv')
+
+# preProcess yelp
 ppy.preProcess()
